3076c04aa9e1a4f4cc5c45e197f5908cb40ab177
Ahumada04 <ahumadae@uci.edu>
2025-03-16 20:47:23 -0700

    finished viewers and genres for most part

--- project.py
+++ project.py
@@ -13,7 +13,7 @@ import mysql.connector
 # pip install pymysql
 db = mysql.connector.connect(host = "127.0.0.1", port = "3306", user="root", password="1234", database = "cs122a")
 dbcursor = db.cursor()
-functions = ["import_"]
+functions = ["import_", "insertViewer", "addGenre"]
 table_names = ['users', 'producers', 'viewers', 'releases', 'movies', 'series', 'videos', 'sessions', 'reviews']
 
 
@@ -23,13 +23,23 @@ def main():
         sys.exit(1)
     else:
         func_name = sys.argv[1]
-        user_data = sys.argv[2]
-        if func_name in globals():
-            globals()[func_name](user_data)
+        if func_name in functions:
+            select_function(func_name)
         else:
             print("Not a function.")
             print(f"Use these instead: {functions}")
 
+
+def select_function(func_name):
+    match func_name:
+        case "import_":
+            import_(sys.argv[2])
+        case "insertViewer":
+            #             [uid:int] [email:str] [nickname:str] [street:str] [city:str] [state:str] [zip:str] [genres:str] [joined_date:date] [first:str] [last:str] [subscription:str]
+            insertViewer(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11],  sys.argv[12], sys.argv[13])
+        case "addGenre":
+            #         [uid:int] [genres:str]
+            addGenre(sys.argv[2], sys.argv[3])
 
 def import_(filepath):
     if os.path.exists(filepath): 
@@ -76,13 +86,76 @@ def reset_db():
         print(f'Error reseting the database: {err}')
 
 
-def insertViewer(viewerInfo):
-    insertQ = f"""" 
-    INSERT INTO viewers (uid, firstname, lastame, subscription)" \
-    VALUES ({viewerInfo});
+#EXAMPLE:         1 test@uci.edu awong "1111 1st street" Irvine CA 92616 "romance;comedy" 2020-04-19 Alice Wong yearly
+def insertViewer(uid, email, nickname, address, city, state, zip, genres, joined, first, last, sub):
+    # TODO
+    # NEED TO UPDATE WITH NULL HANDLING
+    userQ = f"""
+    INSERT INTO users (uid, email, nickname, street, city, state, zip, genres, joined_date) \
+    VALUES ({uid}, "{email}", "{nickname}","{address}", "{city}", "{state}", "{zip}", "{genres}", "{joined}");
     """
-    dbcursor.execute(insertQ)
-    db.commit
+    viewerQ = f"""
+    INSERT INTO viewers (uid, firstname, lastname, subscription) \
+    VALUES ({uid}, "{first}", "{last}", "{sub}");
+    """
+    try:
+        dbcursor.execute(userQ)
+        dbcursor.execute(viewerQ)
+        db.commit()
+    except mysql.connector.Error as err:
+        print(f'Error inserting into users/viewers tables: {err}')
+        return False
+    
+    return True
+
+
+def addGenre(uid, genre):
+    grabQ = f"""
+    SELECT genres 
+    FROM users
+    WHERE uid = {uid}
+    """
+
+    try:
+        dbcursor.execute(grabQ)
+        currGenres = dbcursor.fetchall()
+        if currGenres:
+            newGenres = genre.split(';')
+            currGenres = currGenres[0][0].split(';')
+        else:
+            print("uid not found.")
+            return False
+    except mysql.connector.Error as err:
+        print(f'Unexpected Error: {err}')
+        return False
+    
+    updatedGenres = set(newGenres) | set(currGenres)
+    deliminator = ";"
+    updated = deliminator.join(updatedGenres)
+    # TODO
+    # ADD LOGIC TO STOP IF GENRES IS SAME AS OLD GENRES
+
+    updateQ = f"""
+    UPDATE users 
+    SET genres = "{updated}"
+    WHERE uid = {uid};
+    """
+    
+    dbcursor.execute(updateQ)
+    db.commit()
+
+
+
+
+# translates a list of items to MYSQL syntax
+# def SQLtranslate(dataList):
+#     for i, item in enumerate(dataList):
+#         print(item)
+#         if(item) == 'NULL':
+#             dataList[i] = None
+#         else:
+            
+#     return dataList
 
 
 if __name__ == "__main__":
