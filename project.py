ad532a3ca968a3ec23eb2e0dda1dd885d7e891c8
Ahumada04 <ahumadae@uci.edu>
2025-03-20 20:48:48 -0700

    test 2

--- project.py
+++ project.py
@@ -6,11 +6,11 @@ import mysql.connector
 import csv
 
 # SWITCH TO THIS VERSION WHEN SUBMITING TO GRADESCOPE!!!!!!!!!!!!!!!!!!!!
-# db = mysql.connector.connect(user = 'test', password = 'password', database = 'cs122a')
+db = mysql.connector.connect(user = 'test', password = 'password', database = 'cs122a')
 
 # IF YOU ARE HAVING ISSUES CONNECTING ENTER BELOW INT CMD PROMPT:  
 # pip install pymysql
-db = mysql.connector.connect(host = "127.0.0.1", port = "3306", user="root", password="1234", database = "cs122a")
+# db = mysql.connector.connect(host = "127.0.0.1", port = "3306", user="root", password="1234", database = "cs122a")
 dbcursor = db.cursor()
 functions = ["import", "insertViewer", "addGenre", "listReleases", "popularRelease", "releaseTitle", "activeViewer", "videosViewed", "deleteViewer", "insertSession", "updateRelease", "insertMovie"]
 table_names = ['users', 'producers', 'viewers', 'releases', 'movies', 'series', 'videos', 'sessions', 'reviews']
